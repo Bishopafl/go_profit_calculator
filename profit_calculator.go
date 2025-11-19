03a2be07aaa86c4557946b6b5c5ed5411317910d
Adam Lopez <adam.lopez@hotwiremail.com>
2025-11-18 20:26:34 -0500

    Setting up initial profit calculator in go - will definitely improve this code going forward.

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(`yo`)
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	// send pointer with & before var name
+	fmt.Print("Revenus: ")
+	fmt.Scan(&revenue)
+
+	// send pointer with & before var name
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	// send pointer with & before var name
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
 }
